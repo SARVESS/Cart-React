d58d4812eb82ba9546c3b104d2508dc40d8641ff
SARVESS <sarveshsri76@gmail.com>
2020-08-02 03:32:00 +0530

    add increase quantity func

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React from 'react';
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: '',
+    };
+  }
+  increaseQuantity = () => {
+      console.log('this.state', this.state);
+  }
   render() {
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={ styles.image }/>
+          <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>Phone</div>
-          <div style={ { color: '#777' } }>Rs 999</div>
-          <div style={ { color: '#777' } }>Qty: 1</div>
-          <div className="cart-item-actions">{/* Buttons */}</div>
+          <div style={{ fontSize: 25 }}> {title} </div>
+          <div style={{ color: '#777' }}>Rs. {price} </div>
+          <div style={{ color: '#777' }}>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick= {this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992683.svg"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
+          </div>
         </div>
       </div>
     );
@@ -19,12 +50,12 @@ class CartItem extends React.Component {
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc',
+  },
+};
 
 export default CartItem;
