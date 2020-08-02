6ab1e21df59b1b661e2b5b3520e889c6276bf68d
SARVESS <sarveshsri76@gmail.com>
2020-08-02 06:30:41 +0530

    delete product from cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,48 +1,15 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  //   testing() {
-  //     const promise = new Promise((resolve, reject) => {
-  //       setTimeout(() => {
-  //         resolve('done');
-  //       }, 5000);
-  //     });
-
-  //     promise.then(() => {
-  //       // setState acts like a synchronous call in promise and ajax and hence batching didn't occur here
-  //       this.setState({ qty: this.state.qty + 10 });
-
-  //       console.log('state', this.state);
-  //     });
-  //   }
-  increaseQuantity = () => {
-    //   setState form 1[object form used when prevState is not req.]
-    // this.setState({
-    //   qty: this.state.qty + 1,
-    // }, () = {});
-    // setState acts like an asynchronous call in Event Handlers so batching will be performed here
-
-    // setState form 2[function form used when prevState is req]
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-  };
-
-  decreaseQuantity = () => {
-    this.setState((prevState) => {
-      if (prevState.qty > 0) {
-        return {
-          qty: prevState.qty - 1,
-        };
-      }
-    });
-  };
-
   render() {
     console.log('this.props', this.props);
     const { price, title, qty } = this.props.product;
+    const {
+      product,
+      onDecreaseQuantity,
+      onIncreaseQuantity,
+      onDeleteProduct,
+    } = this.props;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -58,18 +25,19 @@ class CartItem extends React.Component {
               alt="increase"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/992/992651.svg"
-              onClick={this.increaseQuantity}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/992/992683.svg"
-              onClick={this.decreaseQuantity}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
