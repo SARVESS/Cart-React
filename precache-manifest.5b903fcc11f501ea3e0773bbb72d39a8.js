68d8abc23e34104fc427899ea2ccce3e419714df
SARVESS <sarveshsri76@gmail.com>
2020-08-05 04:37:00 +0530

    Updates

---
+++ precache-manifest.5b903fcc11f501ea3e0773bbb72d39a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ce174af14ea4a75eb7c32cab74998d",
+    "url": "/Cart-React/index.html"
+  },
+  {
+    "revision": "3bc2ca6d1df86835a8ef",
+    "url": "/Cart-React/static/css/main.6c11cf9e.chunk.css"
+  },
+  {
+    "revision": "456ba277f52c504a1f86",
+    "url": "/Cart-React/static/js/2.6a833c8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cart-React/static/js/2.6a833c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc2ca6d1df86835a8ef",
+    "url": "/Cart-React/static/js/main.3fe344d6.chunk.js"
+  },
+  {
+    "revision": "9fc654cd8b49f88f70b5",
+    "url": "/Cart-React/static/js/runtime-main.3b59cbb1.js"
+  }
+]);
